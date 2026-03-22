dce970fc7234bc27414a79fe74bb5b55bbc54c9d
DEV MESSY <techdevask@gmail.com>
2026-03-22 20:58:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 export default {
-    PREFIXES: ['.', '!', '/', '$','@','&',':'], // Plusieurs préfixes
+    PREFIXES: ['.'], // Plusieurs préfixes
     BOT_NAME: "ORLYNE",
     OWNER_NUMBER: "24177474264",
     OWNERS: ["24177474264@s.whatsapp.net"],
