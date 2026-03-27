2c1f879bfd3992b197ed526fdd051500f5011db0
DEV MESSY <techdevask@gmail.com>
2026-03-27 11:02:30 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,16 +1,16 @@
 // commands/ping.js
 import { getMessages, formatMessage } from '../Utils/langManager.js';
-import { ReplyRafa } from '../Utils/externalReply.js';
+import { messyFake } from '../Utils/externalReply.js';
 
 export default async function ping(m, dvmsy) {
     try {
         const msg = getMessages(m.sender);
         
         const start = Date.now();
-        await ReplyRafa(msg.ping_wait, dvmsy, m.chat);
+        await messyFake(msg.ping_wait, dvmsy, m.chat);
         const end = Date.now();
         
-        await ReplyRafa(formatMessage(msg.ping, { time: end - start }), dvmsy, m.chat);
+        await messyFake(formatMessage(msg.ping, { time: end - start }), dvmsy, m.chat);
     } catch (error) {
         console.error('Erreur ping:', error);
     }
